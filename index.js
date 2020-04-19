d746c7e60b621dd15279f9e32cc3447111624b3a
thatguy <vaibhavb995@gmail.com>
2020-04-19 02:10:36 -0400

    adding configuration file

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const Router = require('./router');
+const router = require('./router');
+const config = require('./config');
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 });
+
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return Router.requestHandler(request);
+  if (!config['allowed-methods'].includes(request.method)) return new Response(`No handler for ${request.method} call`)
+  return router.requestHandler(request);
 }
